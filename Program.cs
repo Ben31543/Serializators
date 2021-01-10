d8ff0473e064b740269d35c176d23c2c961798e7
Ben Atanesyan <benatanesyan2@gmail.com>
2021-01-10 12:51:52 +0400

    Value Changing For Each Int32 Property

--- Program.cs
+++ Program.cs
@@ -5,20 +5,33 @@ using System.Runtime.Serialization;
 namespace Serializator
 {
     [Serializable]
-    public class User
+    public class User : ISerializable
     {
         public int Age { get; set; }
 
         public string Name { get; set; }
 
         public string Nationality { get; set; }
+
+        [OnSerializing]
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            Type type = typeof(User);
+            MemberInfo[] memberInfo = type.GetMembers();
+
+            for (int i = 0; i < memberInfo.Length; i++)
+            {
+                if (memberInfo[i].GetType().Equals("System.Int32"))
+                    info.GetInt32("Age");
+            }
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            string filePath = "C:\\Users\\benat\\Desktop\\file.bin";
+            string filePath = "C:\\Users\\benat\\Desktop\\file.xml";
             var user = new User
             {
                 Age = 17,
